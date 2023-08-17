24350c662f11abdd2459d8dddead0ec8a87e310a
Aniket Sharma <aniket8439@gmail.com>
2023-08-17 20:09:30 +0530

    Player

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:numeral/numeral.dart';
+import 'package:youtube/pages/player.dart';
+import 'package:youtube/services/api-client.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +12,68 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  ApiClient _apiClient = ApiClient();
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text('Home Screen')));
+    return FutureBuilder(
+        future: _apiClient.getVideos(),
+        builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapShot) {
+          if (!snapShot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapShot.hasError) {
+            return const Center(child: Text('Something went wrong...'));
+          } else {
+            return ListView.builder(
+                itemCount: snapShot.data.length,
+                itemBuilder: (c, int index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (c) =>
+                              Player(snapShot.data[index]['id'])));
+                    },
+                    child: Column(
+                      children: [
+                        Image.network(snapShot.data[index]['snippet']
+                            ['thumbnails']['standard']['url']),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  'https://cdn-icons-png.flaticon.com/256/147/147142.png'),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                                child: Text(
+                              snapShot.data[index]['snippet']['title'],
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            )),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const SizedBox(width: 50),
+                            const Text('Views: '),
+                            Text(Numeral(int.parse(snapShot.data[index]
+                                    ['statistics']['viewCount']))
+                                .format()),
+                            const Text('  | Likes : '),
+                            Text(Numeral(int.parse(snapShot.data[index]
+                                    ['statistics']['likeCount']))
+                                .format()),
+                            const Text(' | '),
+                            Text(timeago.format(DateTime.parse(snapShot
+                                .data[index]['snippet']['publishedAt'])))
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                });
+          }
+        });
   }
 }
